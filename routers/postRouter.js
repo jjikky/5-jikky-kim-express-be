a7815ca133bdda0f593863d85dc48a3a9669580a
jjikky <kok5417@skuniv.ac.kr>
2024-04-20 14:13:10 +0900

    ✨ protect 미들웨어 구현

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { getAllPost } = require('../controllers/postController');
+const { protect } = require('../controllers/authController');
 
 const postRouter = express.Router();
 
-postRouter.get('/', getAllPost);
+postRouter.get('/', protect, getAllPost);
 
 module.exports = postRouter;
